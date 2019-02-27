558c47c0b8c83d8be0cdc2ff6aec0ca13be1c0dd
cslev <cs.lev@gmx.com>
2019-02-27 13:55:48 +0100

    Minor changes to bootstrap

--- ovn_bootstrap.sh
+++ ovn_bootstrap.sh
@@ -3,32 +3,46 @@
 source ovn_config.sh
 
 echo -e "${orange}Installing golang...${none}"
-wget https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz
+if [ -f go1.11.1.linux-amd64.tar.gz ]
+then
+  echo -e "${green}Compressed archive of go1.11.1 is already downloaded...${none}"
+else
+  wget https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz
+fi
 sudo tar -C /usr/local -xzf go1.11.1.linux-amd64.tar.gz
+echo -e "${green}[DONE]${none}"
 
 echo -e "${orange}Installing CNI...${none}"
 pushd ~/
-wget https://github.com/containernetworking/cni/releases/download/v0.6.0/cni-amd64-v0.6.0.tgz
+if [ -f cni-amd64-v0.6.0.tgz ]
+then
+    echo -e "${green}Compressed archive of cni0.6.0 is already downloaded...${none}"
+else
+  wget https://github.com/containernetworking/cni/releases/download/v0.6.0/cni-amd64-v0.6.0.tgz
+fi
+
 sudo mkdir -p /opt/cni/bin
 pushd /opt/cni/bin
 sudo tar -xvzf ~/cni-amd64-v0.6.0.tgz
 popd
 sudo mkdir -p /etc/cni/net.d
 # Create a 99loopback.conf to have atleast one CNI config.
-echo '{
+echo "{
     "cniVersion": "0.2.0",
     "type": "loopback"
-}' | sudo tee /etc/cni/net.d/99loopback.conf
+}" > /etc/cni/net.d/99loopback.conf
 echo -e "${green}[DONE]${none}"
 
 
 echo -e "${orange}Adding docker and kubernetes sources to sources.list...${none}"
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" |  sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo rm -rf /etc/apt/sources.list.d/kubernetes.list
+sudo echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo su -c "echo \"deb https://apt.dockerproject.org/repo ubuntu-xenial main\" >> /etc/apt/sources.list.d/docker.list"
+sudo rm -rf /etc/apt/sources.list.d/docker.list
+sudo su -c "echo \"deb https://apt.dockerproject.org/repo ubuntu-xenial main\" > /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
 echo -e "${green}[DONE]${none}"
 
@@ -45,7 +59,7 @@ echo -e "${green}[DONE]${none}"
 
 echo -e "${orange}Compile GO controller if needed...${none}"
 echo -ne "${orange}Checking existence of directory 'ovn-kubernetes'..."
-if [ -d ovn-kubernetes ]
+if [ -d ./ovn-kubernetes ]
 then
   echo -e "${green}[EXISTS...skipping]${none}"
 else
@@ -58,6 +72,7 @@ else
 fi
 echo -e "${green}[DONE]${none}"
 
+sudo apt-get clean
 
 echo -e "${bold}${yellow}Install openvswitch from source with kernel modules${none}"
 echo -e "Hints:"
