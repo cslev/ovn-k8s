fa6a8bae051332a675a849358919efc2add2f484
cslev <cs.lev@gmx.com>
2019-02-27 11:50:50 +0100

    Add ovn scripts

---
+++ ovn_bootstrap.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+source ovn_config.sh
+
+echo -e "${orange}Installing golang...${none}"
+wget https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.11.1.linux-amd64.tar.gz
+
+echo -e "${orange}Installing CNI...${none}"
+pushd ~/
+wget https://github.com/containernetworking/cni/releases/download/v0.6.0/cni-amd64-v0.6.0.tgz
+sudo mkdir -p /opt/cni/bin
+pushd /opt/cni/bin
+sudo tar -xvzf ~/cni-amd64-v0.6.0.tgz
+popd
+sudo mkdir -p /etc/cni/net.d
+# Create a 99loopback.conf to have atleast one CNI config.
+echo '{
+    "cniVersion": "0.2.0",
+    "type": "loopback"
+}' | sudo tee /etc/cni/net.d/99loopback.conf
+echo -e "${green}[DONE]${none}"
+
+
+echo -e "${orange}Adding docker and kubernetes sources to sources.list...${none}"
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" |  sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo su -c "echo \"deb https://apt.dockerproject.org/repo ubuntu-xenial main\" >> /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+echo -e "${green}[DONE]${none}"
+
+echo -e "${orange}Installing docker...${none}"
+sudo apt-get install -y linux-image-extra-virtual docker-engine
+sudo service docker start
+echo -e "${green}[DONE]${none}"
+
+echo -e "${orange}Installing kubernetes...${none}"
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo service kubelet restart
+echo -e "${green}[DONE]${none}"
+
+
+echo -e "${orange}Compile GO controller if needed...${none}"
+echo -ne "${orange}Checking existence of directory 'ovn-kubernetes'..."
+if [ -d ovn-kubernetes ]
+then
+  echo -e "${green}[EXISTS...skipping]${none}"
+else
+  echo -e "${yellow}ovn-kubernetes does not exists, installing...${none}"
+  git clone http://github.com/openvswitch/ovn-kubernetes
+  cd ovn-kubernetes/go-controller
+  make
+  sudo make install
+  cd ../../
+fi
+echo -e "${green}[DONE]${none}"
+
+
+echo -e "${bold}${yellow}Install openvswitch from source with kernel modules${none}"
+echo -e "Hints:"
+echo -e "${orange}wget https://www.openvswitch.org/releases/openvswitch-2.10.1.tar.gz${none}"
+echo -e "${orange}tar -xzvf openvswitch-2.10.1.tar.gz${none}"
+echo -e "${orange}cd openvswitch-2.10.1${none}"
+echo -e "${orange}./configure --with-linux=/lib/modules/$(uname -r)/build${none}"
+echo -e "${orange}make${none}"
+echo -e "${orange}sudo make install${none}"
+echo -e "${orange}sudo make modules_install${none}"
+echo -e "${green} ---- FINISHED ---- ${none}"
+
+
+
---
+++ ovn_init.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+#this script initializes the kubernetes and docker background for OVN-KUBERNETES
+
+
+source ovn_config.sh
+
+sudo echo
+
+echo -ne "${orange}Cleaning previous attempts' gargabe...${none}"
+sudo pkill etcd
+sudo pkill kube-apiserver
+sudo pkill kube-controller-manager
+sudo pkill kube-scheduler
+sudo rm -rf /var/lib/etcd
+sudo rm -rf /etc/kubernetes/manifests/*
+sudo rm -rf $HOME/.kube
+sudo rm -rf /var/log/openvswitch/ovnkube.log
+sudo rm -rf /etc/kubernetes/*
+
+
+sudo service kubelet stop
+sudo service docker stop
+echo -e "${green}[DONE]${none}"
+
+echo -e "${orange}Recompile GO controller if needed...${none}"
+echo -ne "${orange}Checking existence of directory 'ovn-kubernetes'..."
+if [ -d ovn-kubernetes ]
+then
+  echo -e "${green}[EXISTS...skipping]${none}"
+else
+  echo -e "${yellow}ovn-kubernetes does not exists, installing...${none}"
+  git clone http://github.com/openvswitch/ovn-kubernetes
+  cd ovn-kubernetes/go-controller
+  make
+  sudo make install
+  cd ../../
+fi
+
+
+
+echo -ne "${orange}Starting DOCKER service...${none}"
+sudo service docker start
+echo -e "${green}[DONE]${none}"
+
+
+#echo -ne "${orange}Starting KUBELET service...${none}"
+#sudo service kubelet start
+#echo -e "${green}[DONE]${none}"
+
+
+echo -ne "${orange}Disabling swap...${none}"
+sudo swapoff -a
+echo -e "${green}[DONE]${none}"
+
+echo -e "${orange}Pull kubernetes images...${none}"
+sudo kubeadm config images pull
+echo -e "${green}[DONE]${none}"
+
+
+echo -e "${orange}Initializing kubernetes pod...${none}"
+sudo kubeadm init --pod-network-cidr=$POD_IP_RANGE --apiserver-advertise-address=$OVERLAY_IP \
+        --service-cidr=$SERVICE_IP_RANGE \
+        2>&1 | tee kubeadm.log
+grep "kubeadm join" kubeadm.log | sudo tee kubeadm.log
+echo -e "${green}[DONE]${none}"
+
+echo -ne "${orange}Copy config to $HOME/.kube/config...${none}"
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+echo -e "${green}[DONE]${none}"
+
+# Wait till kube-apiserver is up
+while true; do
+    kubectl get node $NODE_NAME
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    echo -e "${orange}waiting for kube-apiserver to be up${none}"
+    sleep 1
+done
+echo -e "${green}Kube-apiserver is UP!${none}"
+
+
+echo -ne "${orange}Let master run pods too${none}"
+kubectl taint nodes --all node-role.kubernetes.io/master-
+echo -e "${green}[DONE]${none}"
+
+
+
+
+
+
+
+
