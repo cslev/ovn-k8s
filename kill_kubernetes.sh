8bc8ff5f933046a140720724ef5abb6ec0b89073
cslev <cs.lev@gmx.com>
2019-02-27 14:46:01 +0100

    Fix killer script

--- kill_kubernetes.sh
+++ kill_kubernetes.sh
@@ -14,7 +14,7 @@ sudo rm -rf /var/log/openvswitch/ovnkube.log
 
 for i in $(ps aux|grep -v grep|grep -v "kill_kubernetes"|grep kube|awk '{print $2}')
 do
-  kill -9 $i
+  sudo kill -9 $i
 done
 
 # Uncomment this if everything needs to be made from scratch
